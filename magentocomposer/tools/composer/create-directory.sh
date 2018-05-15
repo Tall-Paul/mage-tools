bac84c3575189a24164b79a2700227e55d62e553
Paul Berry <Paul@Alexiss-MacBook-Pro.local>
2018-05-15 15:55:14 +0100

    use standard composer setup

--- magentocomposer/tools/composer/create-directory.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-rm -rf ./vendor/composer
-rm -rf ../http
-mkdir -p ../http
--- rebuild.sh
+++ rebuild.sh
@@ -1,11 +1,5 @@
 #!/bin/bash
 
-#create merged composer.json
-mv composer.json composer-merge.json
-mv magentocomposer/composer.json magentocomposer/composer-merge.json
-
-./bin/jq -s '[.[] | to_entries] | flatten | reduce .[] as $dot ({}; .[$dot.key] += $dot.value)' ./magentocomposer/composer-merge.json ./composer-merge.json  > ./magentocomposer/composer.json
-
 sudo mv http http-old
 sudo rm -rf http-old
 mkdir -p http
@@ -15,31 +9,25 @@ mkdir -p http
 
 #sudo rm -f magentocomposer/composer.lock
 
-cd magentocomposer
 
 echo "Run composer update?  (only if your packages have changed, takes a long time!)"
 read -p "Are you sure? " -n 1 -r
 echo    # (optional) move to a new line
 if [[ $REPLY =~ ^[Yy]$ ]]
 then
-  rm -rfv vendor
-  rm -rfv bin
+  rm -rf vendor
+  rm -rf bin
   rm composer.lock
-  composer update
+  composer update --no-suggest --no-dev
 fi
 
-composer install
+composer install --no-suggest --no-dev
 
-cd ..
+
 cd http
-sudo rm -rfv media
-sudo ln -s ../media/media .
+sudo rm -rf media
+sudo ln -s ../media .
 sudo mkdir var/log
 sudo chmod -R 777 var
 sudo cp ../magentodocker/php/local.xml app/etc
 cd ..
-
-mv magentocomposer/vendor/composer/ http/
-
-mv composer-merge.json composer.json
-mv magentocomposer/composer-merge.json magentocomposer/composer.json
